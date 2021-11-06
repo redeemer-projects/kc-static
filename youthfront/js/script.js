075b733292fa7b989e60ca33f1a871d281dcdea5
Jithin S <jithins41@gmail.com>
2021-11-06 23:56:23 +0530

    Pages updated

---
+++ youthfront/js/script.js
@@ -0,0 +1,13 @@
+var prevScrollpos = window.pageYOffset;
+window.addEventListener("scroll", function () {
+  var tag = document.querySelector(".navbar");
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector('.navbar').style.right = "-100px";
+   
+  }
+  else {
+    document.querySelector('.navbar').style.top= "0px";
+  }
+  prevScrollpos = currentScrollPos;
+})
