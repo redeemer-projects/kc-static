82ea706aaa5cbeed55a7ca06458c0d269a00ff40
Jithin S <jithins41@gmail.com>
2021-11-15 16:03:20 +0530

    Youth front title bar jumping fixed

--- youthfront/js/script.js
+++ youthfront/js/script.js
@@ -1,13 +1,13 @@
-var prevScrollpos = window.pageYOffset;
-window.addEventListener("scroll", function () {
-  var tag = document.querySelector(".navbar");
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector('.navbar').style.right = "-100px";
+// var prevScrollpos = window.pageYOffset;
+// window.addEventListener("scroll", function () {
+//   var tag = document.querySelector(".navbar");
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.querySelector('.navbar').style.right = "-100px";
    
-  }
-  else {
-    document.querySelector('.navbar').style.top= "0px";
-  }
-  prevScrollpos = currentScrollPos;
-})
+//   }
+//   else {
+//     document.querySelector('.navbar').style.top= "0px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// })
